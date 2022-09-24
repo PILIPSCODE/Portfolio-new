2a4f2f0d627a550041e79726b8ef8b871e105529
Pilipus <Pilipus@gmail.com>
2022-09-24 20:31:31 +0700

    updated

--- script.js
+++ script.js
@@ -318,6 +318,32 @@ barba.init({
   ]
 });
 
+ 
+function umur() {
+  var d1 = "29"
+  var m1 = "05"
+  var y1 = "2006"
+  var date = new Date();
+  var d2 = date.getDate();
+  var m2 = 1 + date.getMonth();
+  var y2 = date.getFullYear();
+  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (d1 > d2) {
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+  }
+  if (m1 > m2) {
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
+  var y = y2 - y1;
+  const age = document.querySelector('.age');
+  age.innerHTML = y
+}
+umur()
 
 
-
+const aboutpilip = document.querySelector('.aboutpilip')
+aboutpilip.addEventListener('click',(e) => {
+    aboutpilip.style.transfrom = 'translate-x(50px)'
+})
